7284fac010beb62d09c98b9737aaefa7e6625e65
Umberto Selva <48694886+umbertoselva@users.noreply.github.com>
2023-02-24 00:39:16 +0100

    Update functions.py

--- text_summarizer/functions.py
+++ text_summarizer/functions.py
@@ -2,7 +2,7 @@ import openai
 import streamlit as st
 
 def summarize(prompt):
-    augmented_prompt = f"summarize this text and end with a period: {prompt}"
+    augmented_prompt = f"summarize this text: {prompt}"
     try:
         st.session_state["summary"] = openai.Completion.create(
             model="text-davinci-003",
