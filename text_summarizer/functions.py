b7cf85c98d175f6fe3e613e7f8823d8bf79c2cfe
Umberto Selva <48694886+umbertoselva@users.noreply.github.com>
2023-02-24 23:31:55 +0100

    Update functions.py

--- text_summarizer/functions.py
+++ text_summarizer/functions.py
@@ -2,14 +2,13 @@ import openai
 import streamlit as st
 
 def summarize(prompt):
-    augmented_prompt = f"summarize this text: {prompt}"
+    augmented_prompt = f"Summarize the following text in only one single sentence: {prompt}"
     try:
         st.session_state["summary"] = openai.Completion.create(
             model="text-davinci-003",
             prompt=augmented_prompt,
             temperature=.5,
-            max_tokens=200,
-            stop=". ",
+            max_tokens=500,
         )["choices"][0]["text"]
     except:
         st.write('There was an error =(')
